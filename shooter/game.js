22b423e31b2d9f393283fa58e8adf270b9054fbf
Drake Griffith <drakegriffith8@gmail.com>
2015-07-11 11:29:23 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -1,1 +1,16 @@
-console.log("hi");
+'use strict';
+
+var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function render() {
+}
